5f63e29c543e8f980a10295f0cacaf7c87616fa8
Sam Chen <samchen63@users.noreply.github.com>
2019-06-18 14:47:47 +1000

    Update AddCourseTests.java

--- SeleniumWebDriverExampleProjectJava/src/test/java/junittest/AddCourseTests.java
+++ SeleniumWebDriverExampleProjectJava/src/test/java/junittest/AddCourseTests.java
@@ -1,11 +1,3 @@
-//-----------------------------------------------------------------------------
-// <copyright file="AddCourseTests.java" company="Planit Testing">
-//      Copyright © 2019 Planit Testing.
-//      All rights reserved.
-// </copyright>
-// <created>09/04/2019</created>
-// <author>Sam Chen</author>
-//-----------------------------------------------------------------------------
 package junittest;
 
 import java.io.BufferedReader;
