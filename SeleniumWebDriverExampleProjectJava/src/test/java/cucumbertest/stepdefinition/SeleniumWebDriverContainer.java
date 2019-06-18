e7ed2005cccf8963071bccbe0e34bb871ce77a7e
Sam Chen <samchen63@users.noreply.github.com>
2019-06-18 14:46:52 +1000

    Update SeleniumWebDriverContainer.java

--- SeleniumWebDriverExampleProjectJava/src/test/java/cucumbertest/stepdefinition/SeleniumWebDriverContainer.java
+++ SeleniumWebDriverExampleProjectJava/src/test/java/cucumbertest/stepdefinition/SeleniumWebDriverContainer.java
@@ -1,11 +1,3 @@
-//-----------------------------------------------------------------------------
-// <copyright file="SeleniumWebDriverContainer.java" company="Planit Testing">
-//      Copyright © 2019 Planit Testing.
-//      All rights reserved.
-// </copyright>
-// <created>10/04/2019</created>
-// <author>Sam Chen</author>
-//-----------------------------------------------------------------------------
 package cucumbertest.stepdefinition;
 
 import org.openqa.selenium.WebDriver;
