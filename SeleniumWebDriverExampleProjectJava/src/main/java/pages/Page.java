ca2a2331c6b69d14218f59ba48f69d14643aeae1
Sam Chen <samchen63@users.noreply.github.com>
2019-06-18 14:52:16 +1000

    Update Page.java

--- SeleniumWebDriverExampleProjectJava/src/main/java/pages/Page.java
+++ SeleniumWebDriverExampleProjectJava/src/main/java/pages/Page.java
@@ -1,11 +1,3 @@
-//-----------------------------------------------------------------------------
-//<copyright file="Page.java" company="Planit Testing">
-//   Copyright © 2019 Planit Testing.
-//   All rights reserved.
-//</copyright>
-//<created>08/04/2019</created>
-//<author>Sam Chen</author>
-//-----------------------------------------------------------------------------
 package pages;
 
 import org.openqa.selenium.WebDriver;
